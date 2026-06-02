f18ec5749aacba4ab12d15fe0371d3ce209fd2e6
Lucho <quispeortizluis00@gmail.com>
2026-06-02 10:33:03 -0400

    arreglando group1

--- backend_fam/src/controllers/miembro.controller.js
+++ backend_fam/src/controllers/miembro.controller.js
@@ -114,7 +114,7 @@ export const getMiembrosByAsociacion = async (req, res) => {
 
     // Campos básicos para todos los usuarios (Vista Pública)
     const attributes = [
-      'id', 'id_asociacion', 'alias', 'nombre', 'municipio',
+      'id', 'id_asociacion', 'alias', 'nombre', 'tipo_miembro', 'municipio',
       'telefono_publico', 'telefono_fax', 'correo_publico', 'direccion', 'foto'
     ];
 
